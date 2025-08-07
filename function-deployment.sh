d870b798efb23f4b14983ecb72483ff64a112a4b
Alberto Farias <cafaray@gmail.com>
2025-08-08 00:14:52 +0200

    removing unnecesary files

--- function-deployment.sh
+++
@@ -1,9 +0,0 @@
-gcloud functions deploy venue-searchpos-service \
-  --gen2 \
-  --runtime go122 \
-  --entry-point EntryPoint \
-  --source . \
-  --trigger-http \
-  --region europe-southwest1 \
-  --allow-unauthenticated \
-  --set-secrets MONGO_URI=mongodb-supporters-uri:latest
